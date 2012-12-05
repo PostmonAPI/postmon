430ff87907e104f25ef770b4b51aa1c51c8ed9a3
Alê Borba <alexandre.borba@imasters.com.br>
2012-12-05 10:44:03 -0200

    Correção do bug para mais de um resultado (Igor Hercowitz)

--- correios.py
+++ correios.py
@@ -4,13 +4,13 @@
 # author: Alexandre Borba
 #         Igor Hercowitz
 #
-# v 1.0
+# v 1.1
 # usage:
 # >>> tracker = CepTracker()
 # >>> infos = tracker.track(cep)
 ####################################################################
 
-from lxml.html import fromstring
+
 import requests
 import unicodedata
 import re
@@ -18,23 +18,41 @@ import re
 class CepTracker():
 	def __init__(self):
 		self.url = 'http://m.correios.com.br/movel/buscaCepConfirma.do'
-		self.result = dict()
+		self.result = []
+
 		self.fields = ['logradouro', 'bairro', ['cidade', 'estado'], 'cep']
 
-	def track(self, cep):
+	def _get_infos_(self, cep, csspattern='div .respostadestaque'):
+		from lxml.html import fromstring
 		response = requests.post(self.url, 
 			                 data={'cepEntrada': cep, 'tipoCep':'', 'cepTemp':'', 'metodo':'buscarCep'})
 
 		html = fromstring(response.text)
-		itens = html.cssselect('div .respostadestaque')
+		return html.cssselect(csspattern)
 
-		for index, item in enumerate(itens):
+	def track(self, cep):
+		itens = self._get_infos_(cep)
+
+		index=0
+		for item in itens:
+
+			if index % 4 == 0:
+				if index > 0:
+					index = 0
+					self.result.append(data)
+
+				data = dict()
+
 			text = re.sub('\s+',' ',item.text.strip())
 
 			if (index == 2):
 				for j, text in enumerate(text.split('/')):
-					self.result[self.fields[index][j]] = unicodedata.normalize('NFKD', text.strip()).encode('ascii','ignore') if isinstance(text, unicode) else text.strip() 
-			else:	
-				self.result[self.fields[index]] = unicodedata.normalize('NFKD', text).encode('ascii','ignore') if isinstance(text, unicode) else text 
+					data[self.fields[index][j]] = unicodedata.normalize('NFKD', text.strip()).encode('ascii','ignore') if isinstance(text, unicode) else text.strip()
+			else:
+				data[self.fields[index]] = unicodedata.normalize('NFKD', text).encode('ascii','ignore') if isinstance(text, unicode) else text
+
+			index +=1
+
+		self.result.append(data)
 
 		return self.result
