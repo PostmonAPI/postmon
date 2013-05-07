869eac740ec057c61dbe2ad7bd12fb9ee0851f59
Alê Borba <alexandre.borba@imasters.com.br>
2013-05-06 21:51:01 -0300

    Retornando com o import correios.
    Ele controla o track de encomendas.

--- PostmonServer.py
+++ PostmonServer.py
@@ -3,7 +3,7 @@ import json
 from bottle import route, run, response
 from CepTracker import CepTracker
 from requests import ConnectionError
-
+from correios import Correios
 from database import MongoDb as Database
 
 app_v1 = bottle.Bottle()
