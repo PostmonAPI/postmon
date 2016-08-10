d1fba97cafdeca96e691692a8f1233dece22306e
Iuri de Silvio <iurisilvio@gmail.com>
2016-08-10 09:40:32 -0300

    Implementação inicial da busca ativa de pacotes.

--- PackTracker.py
+++ PackTracker.py
@@ -1,7 +1,11 @@
 # coding: utf-8
+import json
 import os
 
 import packtrack
+import requests
+
+from database import MongoDb as Database
 
 
 def correios(track, backend=None):
@@ -23,3 +27,46 @@ def correios(track, backend=None):
         }
         result.append(historico)
     return result
+
+
+def register(provider, track, callback):
+    """
+    Registra o pacote para acompanhamento.
+    """
+    db = Database()
+    return db.packtrack.register(provider, track, callback)
+
+
+def run(provider, track):
+    db = Database()
+    obj = db.packtrack.get_one(provider, track)
+
+    if provider != 'ect':
+        raise ValueError(u"Unexpected provider: %s" % provider)
+
+    try:
+        data = correios(track)
+    except ValueError:
+        return False
+
+    changed = obj.get('historico') != data.get('historico')
+    db.packtrack.update(provider, track, data, changed=changed)
+    return changed
+
+
+def report(provider, track):
+    db = Database()
+    obj = db.packtrack.get_one(provider, track)
+
+    _meta = obj.pop('_meta')
+    callbacks = _meta.pop('callbacks')
+
+    for callback in callbacks:
+        data = obj.copy()
+        url = callback['callback']
+        data['input'] = callback
+        headers = {'Content-Type': 'application/json'}
+        requests.post(
+            url,
+            headers=headers,
+            data=json.dumps(data))
